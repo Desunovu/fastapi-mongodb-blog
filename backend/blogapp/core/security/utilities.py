2dd8ac59276588e3ce36f2434a7a7730221b97ce
Kirill Tatarinov <desunovu@gmail.com>
2023-08-04 18:11:03 +0300

    style: отформатировать пакет security (2)

--- backend/blogapp/core/security/utilities.py
+++ backend/blogapp/core/security/utilities.py
@@ -31,8 +31,7 @@ async def get_user_by_username(username: str) -> UserDocument | None:
     return user
 
 
-async def authenticate_user(username: str,
-                            password: str) -> UserDocument | None:
+async def authenticate_user(username: str, password: str) -> UserDocument | None:
     """Аутентифицирует пользователя по username и password"""
     user = await get_user_by_username(username)
     if not user:
@@ -42,22 +41,20 @@ async def authenticate_user(username: str,
     return user
 
 
-def create_access_token(data: dict,
-                        expires_delta: timedelta | None = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     """Создает токен доступа с данными из словаря"""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 
 async def get_current_user(
-        token: Annotated[str, Depends(oauth2_scheme)]
+    token: Annotated[str, Depends(oauth2_scheme)]
 ) -> UserDocument:
     """Зависимость - возвращает пользователя по переданному токену"""
     credentials_exception = HTTPException(
@@ -66,8 +63,7 @@ async def get_current_user(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token=token, key=SECRET_KEY,
-                             algorithms=[ALGORITHM])
+        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -80,7 +76,7 @@ async def get_current_user(
 
 
 async def get_active_current_user(
-        current_user: Annotated[UserDocument, Depends(get_current_user)]
+    current_user: Annotated[UserDocument, Depends(get_current_user)]
 ) -> UserDocument:
     """Зависимость - возвращает активного пользователя"""
     if current_user.disabled:
@@ -93,14 +89,11 @@ class RoleChecker:
         self.allowed_role = allowed_role
 
     def __call__(
-            self, current_user: Annotated[
-                UserDocument, Depends(get_active_current_user)]
+        self, current_user: Annotated[UserDocument, Depends(get_active_current_user)]
     ) -> UserDocument:
         all_roles = RolesEnum.all_roles()
-        if all_roles.index[current_user.role] > all_roles.index[
-            self.allowed_role]:
+        if all_roles.index[current_user.role] > all_roles.index[self.allowed_role]:
             raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Access forbidden"
+                status_code=status.HTTP_403_FORBIDDEN, detail="Access forbidden"
             )
         return current_user
