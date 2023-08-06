4241c6b52ab0058bc73f7d3e2804d991391bd402
desunovu <desunovu@gmail.com>
2023-08-06 03:42:48 +0300

    fix: разделить модель коллекции и ответа на запрос
    - выделить класс UserBaseModel, возвращать его в запросах
    - применить зависимость  Проверка роли в тестовых роутах

--- backend/blogapp/modules/api_test.py
+++ backend/blogapp/modules/api_test.py
@@ -3,23 +3,24 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from .tags.model import TagDocument
-from .users.model import UserDocument
-from ..core.security.utilities import get_active_current_user
+from .users.model import UserDocument, UserBaseModel
+from ..core.security.roles import RolesEnum
+from ..core.security.utilities import RoleChecker
 
-router = APIRouter(
-    prefix="/test"
-)
+router = APIRouter(prefix="/test")
 
 
 @router.get("/users/me")
 async def test_users(
-        current_user: Annotated[UserDocument, Depends(get_active_current_user)]
+    current_user: Annotated[UserDocument, Depends(RoleChecker(RolesEnum.READER.value))]
 ):
-    return current_user
+    return UserBaseModel(**current_user.dict())
 
 
 @router.post("/add_some_documents")
-async def add_some_documents():
+async def add_some_documents(
+    current_user: Annotated[UserDocument, Depends(RoleChecker(RolesEnum.ADMIN.value))]
+):
     tag = TagDocument(name="New tag")
     await TagDocument.insert_one(tag)
 
