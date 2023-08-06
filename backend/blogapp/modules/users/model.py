012e707dd4bf6e139fb10118755859e8503ecaeb
desunovu <desunovu@gmail.com>
2023-08-06 03:42:48 +0300

    fix: разделить модель коллекции и ответа на запрос
    - выделить класс UserBaseModel, возвращать его в запросах
    - применить зависимость  Проверка роли в тестовых роутах

--- backend/blogapp/modules/users/model.py
+++ backend/blogapp/modules/users/model.py
@@ -1,19 +1,24 @@
 from datetime import datetime
 
 from beanie import Document
-from pydantic import EmailStr
+from pydantic import EmailStr, BaseModel
 
 from ...core.security.roles import RolesEnum
 
 
-class UserDocument(Document):
+class UserBaseModel(BaseModel):
+    """Базовая модель пользователя"""
+
     username: str
-    password_hash: str
     email: EmailStr
     role: RolesEnum | None = None
     disabled: bool | None = None
     created_at: datetime | None = None
     updated_at: datetime | None = None
+
+
+class UserDocument(UserBaseModel, Document):
+    password_hash: str
 
     class Settings:
         name = "users"
