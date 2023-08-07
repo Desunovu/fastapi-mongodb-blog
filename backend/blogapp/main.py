f64210a4c893f02b1cffba35332509ef49631c10
desunovu <desunovu@gmail.com>
2023-08-08 00:55:41 +0300

    feat: настроить логгирование

--- backend/blogapp/main.py
+++ backend/blogapp/main.py
@@ -1,16 +1,24 @@
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 
+from .core.logging import init_loggers
 from .core.mongodb import init_odm
 from .core.security import routes as security_routes
 from .modules import api_test
 
+init_loggers()
+
 app = FastAPI()
+
+log = logging.getLogger("blogapp")
 
 
 @app.on_event("startup")
 async def startup():
     await init_odm()
+    log.info("Выполнены задачи startup event'а")
 
 
 app.include_router(api_test.router)
