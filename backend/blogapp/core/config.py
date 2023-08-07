b8e5423057482d591e0300422be510d7e4456a61
desunovu <desunovu@gmail.com>
2023-08-08 00:55:41 +0300

    feat: настроить логгирование

--- backend/blogapp/core/config.py
+++ backend/blogapp/core/config.py
@@ -1,11 +1,13 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 # Загрузка переменных окружения из файла .envexample
 # без перезаписи если они уже существуют
 dotenv_path = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), '.envexample'
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+    ".envexample",
 )
 load_dotenv_result = load_dotenv(dotenv_path=dotenv_path)
 
@@ -17,3 +19,7 @@ MONGODB_DATABASE = os.getenv("MONGODB_DATABASE")
 # Прочие константы
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Вычисление абсолютных путей приложения
+BACKEND_DIR_PATH: Path = Path().parent.parent.absolute()
+LOGS_DIR_PATH: Path = BACKEND_DIR_PATH / "logs"
---
+++ backend/blogapp/core/logging.py
@@ -0,0 +1,44 @@
+import time
+from logging.config import dictConfig
+
+from pydantic import BaseModel
+
+from .config import LOGS_DIR_PATH
+
+
+# From https://stackoverflow.com/questions/63510041/adding-python-logging-to-fastapi-endpoints-hosted-on-docker-doesnt-display-api
+class LogConfig(BaseModel):
+    """Конфигурация логгера"""
+
+    LOGGER_NAME: str = "blogapp"
+    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
+    LOG_LEVEL: str = "DEBUG"
+    LOGS_DIR: str = LOGS_DIR_PATH
+
+    # Logging config
+    version = 1
+    disable_existing_loggers = False
+    formatters = {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": LOG_FORMAT,
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    }
+    handlers = {
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "default",
+            "filename": LOGS_DIR_PATH / f"""{time.strftime("%Y%m%d-%H%M%S")}.log""",
+        },
+    }
+    loggers = {
+        LOGGER_NAME: {"handlers": ["file"], "level": LOG_LEVEL},
+    }
+
+
+def init_loggers():
+    if not LOGS_DIR_PATH.exists():
+        LOGS_DIR_PATH.mkdir()
+
+    dictConfig(LogConfig().dict())
