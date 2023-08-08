4ef700a004cdf0a32e1349a8d4c54fa663c10de9
desunovu <desunovu@gmail.com>
2023-08-08 15:44:24 +0300

    fix: перенести запуск приложения из файла на уровень основных пакетов
    исправляет контекст относительных импортов

--- backend/blogapp/main.py
+++ backend/blogapp/main.py
@@ -1,6 +1,5 @@
 import logging
 
-import uvicorn
 from fastapi import FastAPI
 
 from .core.logging import init_loggers
@@ -23,6 +22,3 @@ async def startup():
 
 app.include_router(api_test.router)
 app.include_router(security_routes.router)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
