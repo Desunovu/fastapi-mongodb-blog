d6ad5f4815119d300aa39d292ec2f4ec7b897d86
desunovu <desunovu@gmail.com>
2023-08-08 18:08:55 +0300

    doc: исправить grammar errors

--- backend/blogapp/core/config.py
+++ backend/blogapp/core/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-# Загрузка переменных окружения из файла .envexample
+# Загрузка переменных окружения из файла ".envexample"
 # без перезаписи если они уже существуют
 dotenv_path = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
