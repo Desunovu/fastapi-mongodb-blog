0ba4480f0fb1adccf7e03ee3fc2db782aa373770
desunovu <desunovu@gmail.com>
2023-08-18 02:34:09 +0300

    doc: добавить документстроки и комментарии к article routes

--- backend/blogapp/modules/articles/routes.py
+++ backend/blogapp/modules/articles/routes.py
@@ -21,12 +21,16 @@ async def create_article(
         UserDocument, Depends(RoleChecker(allowed_role=RolesEnum.AUTHOR.value))
     ],
 ):
+    """Создает новую статью."""
+    # Создание документа
     article = ArticleDocument(
         author=current_user,
         created_at=datetime.utcnow(),
         **article_data.model_dump(),
     )
+    # Вставка документа
     await ArticleDocument.insert_one(article)
+
     return {"article": article}
 
 
@@ -37,6 +41,9 @@ async def read_article(
         UserDocument, Depends(RoleChecker(allowed_role=RolesEnum.READER.value))
     ],
 ):
+    """Возвращает статью по ее uuid."""
+
+    # Получение данных
     article = await ArticleDocument.get(document_id=article_id)
     if not article:
         raise HTTPException(
@@ -53,11 +60,13 @@ async def update_article(
         UserDocument, Depends(RoleChecker(allowed_role=RolesEnum.AUTHOR.value))
     ],
 ):
-    # Получить документ статьи
+    """Обновляет статью по ее uuid"""
+
+    # Получение данных
     article = await ArticleDocument.get_or_404(document_id=article_id, fetch_links=True)
-    # Проверить права редактирования
+    # Проверка прав
     _current_user = check_user_can_modify_article(article=article, user=current_user)
-    # Обновить поля и сохранить документ
+    # Обновление данных
     article = article.model_copy(update=article_data.model_dump(exclude_unset=True))
     article.updated_at = datetime.utcnow()
     await article.save()
@@ -67,12 +76,23 @@ async def update_article(
 
 @router.delete("/{article_id}")
 async def delete_article(
-        article_id: PydanticObjectId,
-        current_user: Annotated[UserDocument, Depends(RoleChecker(allowed_role=RolesEnum.AUTHOR.value))]
+    article_id: PydanticObjectId,
+    current_user: Annotated[
+        UserDocument, Depends(RoleChecker(allowed_role=RolesEnum.AUTHOR.value))
+    ],
 ):
-    article = await ArticleDocument.get_or_404(document_id=article_id, fetch_links=False)
+    """Удаляет статью по ее uuid"""
+
+    # Получение данных
+    article = await ArticleDocument.get_or_404(
+        document_id=article_id, fetch_links=False
+    )
+    # Проверка прав
     _current_user = check_user_can_modify_article(article=article, user=current_user)
+    # Удаление данных
     delete_result = await article.delete()
+
     if delete_result:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
+    # Исключение на случай если не получен delete_result
     raise HTTPException(status_code=status.HTTP_410_GONE)
