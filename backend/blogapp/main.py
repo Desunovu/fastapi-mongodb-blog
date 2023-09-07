b5f7864c25052a05a44dfb46550daea760abdbb8
desunovu <desunovu@gmail.com>
2023-09-07 21:52:17 +0300

    chore: удалить api_test.py

--- backend/blogapp/main.py
+++ backend/blogapp/main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 from .core.database.mongodb import init_odm
 from .core.logging import init_loggers
 from .core.security import routes as security_routes
-from .modules import api_test
 from .modules.articles.routes import router as articles_router
 from .modules.comments.routes import router as comments_router
 
@@ -22,7 +21,6 @@ async def startup():
     log.info("Выполнены задачи startup event'а")
 
 
-app.include_router(api_test.router)
 app.include_router(security_routes.router)
 app.include_router(articles_router)
 app.include_router(comments_router)
