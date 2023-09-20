0476e4a689470a08245ba6697fb40e2c47477d62
desunovu <desunovu@gmail.com>
2023-09-21 02:57:00 +0300

    feat(frontend): добавить ArticleView

---
+++ frontend/src/components/articles/SingleArticle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { type ArticleDocument } from '@/client'
+import UserItemSection from '@/components/users/UserItemSection.vue'
+
+const props = defineProps<{
+  article: ArticleDocument | undefined
+}>()
+</script>
+
+<template>
+  <q-item class="row items-start bg-secondary">
+    <UserItemSection />
+
+    <q-item-section class="col column items-start">
+      <q-item-label header lines="1" class="text-h5">
+        {{ article?.title }}
+      </q-item-label>
+      <q-item-label caption class="text-body">{{ article?.created_at }}</q-item-label>
+      <q-item-label class="text-body1 text-white">{{ article?.content }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
---
+++ frontend/src/views/ArticleView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { DefaultService, type ArticleDocument } from '@/client'
+import SingleArticle from '@/components/articles/SingleArticle.vue'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const article = ref<ArticleDocument>()
+
+const route = useRoute()
+
+onMounted(async () => {
+  const articleResponse = await DefaultService.readArticleArticlesArticleIdGet(
+    route.params.id.toString()
+  )
+  article.value = articleResponse.article
+})
+</script>
+
+<template>
+  <q-page padding>
+    <SingleArticle :article="article" />
+  </q-page>
+</template>
