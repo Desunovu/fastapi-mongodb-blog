40cbe9853e5f3e31ebca8c9a4759053c782e1aee
desunovu <desunovu@gmail.com>
2023-09-29 20:44:40 +0300

    fix(frontend): добавить props small к UserItemSection.vue

--- frontend/src/components/users/UserItemSection.vue
+++ frontend/src/components/users/UserItemSection.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
+export interface Props {
+  small?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  small: () => false
+})
+
+const avatarSize = computed(() => {
+  return props.small ? '50px' : '100px'
+})
+
+const sectionWidth = computed(() => {
+  return props.small ? '70px' : '20%'
+})
 </script>
 
 <template>
-  <q-item-section avatar class="column items-center q-pt-lg" style="min-width: 20%; max-width: 20%">
-    <q-avatar size="100px" rounded>
+  <q-item-section
+    avatar
+    class="column items-center justify-start"
+    :style="{ 'min-width': sectionWidth, 'max-width': sectionWidth }"
+  >
+    <q-avatar :size="avatarSize" rounded>
       <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
     </q-avatar>
-    <q-item-label class="text-body ellipsis text-white">
+    <q-item-label v-if="!small" class="text-body ellipsis text-white">
       Лололошка | Официальный канал
     </q-item-label>
   </q-item-section>
