996eff8c00ea96b65bbf52786f575d2bf9ce1d3b
desunovu <desunovu@gmail.com>
2023-10-10 05:04:06 +0300

    feat(frontend): добавить кнопку удаления статей

--- frontend/src/components/articles/ArticleEditor.vue
+++ frontend/src/components/articles/ArticleEditor.vue
@@ -21,6 +21,7 @@ const titleRef = ref<string>(
 const conentRef = ref<string>(
   props.articleToEdit?.content ? props.articleToEdit.content : 'Текст статьи'
 )
+const articleDeleteDialog = ref<boolean>(false)
 
 async function handleArticleSave() {
   if (props.createMode) {
@@ -33,11 +34,17 @@ async function handleArticleSave() {
   } else if (props.editMode) {
     const updateArticleResponse = await DefaultService.updateArticleArticlesArticleIdPut(
       props.articleToEdit?._id ?? '',
-      { title: titleRef.value, content: conentRef.value}
+      { title: titleRef.value, content: conentRef.value }
     )
     Notify.create('Cтатья успешно обновлена, перенаправляем')
     router.push('/article/' + updateArticleResponse.article._id)
   }
+}
+
+async function handleArticleDelete() {
+  await DefaultService.deleteArticleArticlesArticleIdDelete(props.articleToEdit?._id!)
+  Notify.create('Статья удалена')
+  router.push('/')
 }
 </script>
 
@@ -51,18 +58,32 @@ async function handleArticleSave() {
         class="q-ma-md"
       />
       <q-editor v-model="conentRef" min-height="5rem" />
-
-      <q-btn label="Сохранить статью" class="q-ma-md self-stretch" @click="handleArticleSave" />
-
-      <!-- <q-card flat bordered>
-      <q-card-section>
-        <pre style="white-space: pre-line">{{ editor }}</pre>
-      </q-card-section>
+      <q-card-actions class="row justify-between">
+        <q-btn label="Сохранить статью" class="q-ma-md self-stretch" @click="handleArticleSave" />
+        <!-- Красная кнопка удалить вызывающая диалог удаления -->
+        <q-btn
+          :if="editMode"
+          label="Удалить статью"
+          class="q-ma-md self-stretch bg-negative"
+          @click="articleDeleteDialog = true"
+        />
+      </q-card-actions>
     </q-card>
 
-    <q-card flat bordered>
-      <div v-html="editor" />
-    </q-card> -->
-    </q-card>
+    <q-dialog v-model="articleDeleteDialog">
+      <!-- Диалог подтвержения удаления статьи (Сообщение: вы уверены что хотите удалить статью? Это действие необратимо) -->
+      <q-card class="bg-secondary items-center" style="width: 100%">
+        <q-card-section>
+          <div class="text-body2 text-center q-mt-sm">
+            Вы уверены что хотите удалить статью?<br />
+            Это действие <b class="text-info">необратимо</b>.
+          </div>
+        </q-card-section>
+        <q-card-actions class="justify-around">
+          <q-btn @click="articleDeleteDialog = false" color="primary">Не удалять</q-btn>
+          <q-btn @click="handleArticleDelete" color="secondary">Удалить</q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
