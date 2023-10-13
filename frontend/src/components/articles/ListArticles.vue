1476514509be95b95f83bb7a75b76a6bb1170c89
desunovu <desunovu@gmail.com>
2023-10-13 04:58:19 +0300

    feat(frontend): добавить поисковую строку

--- frontend/src/components/articles/ListArticles.vue
+++ frontend/src/components/articles/ListArticles.vue
@@ -1,44 +1,103 @@
 <script setup lang="ts">
 import { type ArticleDocument } from '@/client'
 import UserItemSection from '@/components/users/UserItemSection.vue'
+import { ref } from 'vue'
+import { onBeforeMount } from 'vue'
+import { DefaultService } from '@/client'
 
-const props = defineProps({
-  articles: Array<ArticleDocument>
+const articles = ref<ArticleDocument[]>([])
+const searchText = ref('')
+const freezedSearchText = ref('')
+const limit = ref<number>(10)
+const skip = ref<number>(0)
+
+const loadMoreArticles = async () => {
+  const articlesResponse = await DefaultService.listArticlesArticlesGet(
+    skip.value,
+    limit.value,
+    undefined,
+    undefined,
+    undefined,
+    freezedSearchText.value.length > 0 ? freezedSearchText.value : undefined
+  )
+  articles.value = articlesResponse.articles
+}
+
+const handleSearchbarSubmit = async () => {
+  articles.value = []
+  skip.value = 0
+  freezedSearchText.value = searchText.value
+  loadMoreArticles()
+}
+
+const handleSearchbarClear = () => {
+  searchText.value = ''
+  freezedSearchText.value = ''
+  loadMoreArticles()
+}
+
+onBeforeMount(async () => {
+  loadMoreArticles()
 })
 </script>
 
 <template>
-  <q-list separator>
-    <q-item
-      v-for="article in articles"
-      :key="article._id ?? ''"
-      class="row items-start bg-secondary"
+  <div class="column items-center bg-secondary">
+    <!-- Поисковая строка -->
+    <q-input
+      v-model="searchText"
+      @keyup.enter="handleSearchbarSubmit"
+      placeholder="Поиск"
+      class="bg-secondary search-input q-py-md q-px-sm"
     >
-      <UserItemSection :user="article.author"/>
+      <template v-slot:append>
+        <q-icon name="search" @click="handleSearchbarSubmit" class="cursor-pointer" />
+        <q-icon name="close" @click="handleSearchbarClear" class="cursor-pointer" />
+      </template>
+    </q-input>
+    <!-- Информация о поиске-->
+    <div v-if="freezedSearchText.length > 0" class="row self-sta text-info q-mr-md q-mb-lg">
+      <div class="q-mr-md">Поисковый запрос: {{ freezedSearchText }}</div>
+      <div>Найдено статей: {{ articles.length }}</div>
+    </div>
 
-      <q-item-section class="col column items-start">
-        <q-item-label header lines="1" class="text-h5">
-          <router-link
-            style="text-decoration: none; color: inherit"
-            :to="'/article/' + article._id"
-            >{{ article.title }}</router-link
-          >
-        </q-item-label>
-        <q-item-label
-          lines="5"
-          class="text-body1 text-white"
-          v-html="article.content!.replace(/\n/g, '<br>')"
-        />
-        <q-btn :to="'/article/' + article._id" flat class="float-right">
-          <q-item-label caption class="text-white">Открыть статью полностью >>></q-item-label>
-        </q-btn>
-      </q-item-section>
-    </q-item>
-  </q-list>
+    <!-- Список статей -->
+    <q-list separator>
+      <q-item
+        v-for="article in articles"
+        :key="article._id ?? ''"
+        class="row items-start bg-secondary"
+      >
+        <UserItemSection :user="article.author" />
+
+        <q-item-section class="col column items-start">
+          <q-item-label header lines="1" class="text-h5">
+            <router-link
+              style="text-decoration: none; color: inherit"
+              :to="'/article/' + article._id"
+              >{{ article.title }}</router-link
+            >
+          </q-item-label>
+          <q-item-label
+            lines="5"
+            class="text-body1 text-white"
+            v-html="article.content!.replace(/\n/g, '<br>')"
+          />
+          <q-btn :to="'/article/' + article._id" flat class="float-right">
+            <q-item-label caption class="text-white">Открыть статью полностью >>></q-item-label>
+          </q-btn>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <style>
 .a {
   text-decoration: none;
+}
+
+.search-input {
+  width: 80%;
 }
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { DefaultService, type ArticleDocument } from '@/client'
 import ListArticles from '@/components/articles/ListArticles.vue'
-import { onMounted, ref } from 'vue'
-
-const articles = ref<ArticleDocument[]>([])
-
-onMounted(async () => {
-  const articlesResponse = await DefaultService.listArticlesArticlesGet()
-  articles.value = articlesResponse.articles
-})
 </script>
 
 <template>
   <q-page padding>
-    <ListArticles :articles="articles" />
+    <ListArticles />
   </q-page>
 </template>
