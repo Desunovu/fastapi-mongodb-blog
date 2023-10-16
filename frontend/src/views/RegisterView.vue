ef9c8d10885154d44b2b40f0a2f782747306d485
desunovu <desunovu@gmail.com>
2023-10-16 09:21:25 +0300

    feat(frontend): добавить страницу регистрации

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RegisterForm from '@/components/RegisterForm.vue'
+</script>
+
+<template>
+  <q-page padding class="column items-center">
+    <RegisterForm />
+  </q-page>
+</template>
