95c60497be9b50ba9bc69caa5d09baf4948a6a23
desunovu <desunovu@gmail.com>
2023-10-16 09:13:04 +0300

    docs(frontend): добавить комментарии к роутам в роутере

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
 import HomeView from '@/views/HomeView.vue'
 import LogoutView from '@/views/LogoutView.vue'
 import ArticleView from '@/views/ArticleView.vue'
@@ -12,15 +13,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
+    // LOGIN AND REGISTER
     { path: '/login', name: 'login', component: LoginView },
+    { path: '/register', name: 'register', component: RegisterView },
     { path: '/logout', name: 'logout', component: LogoutView },
+    // ARTICLES
     { path: '/create-article', name: 'create-article', component: EditArticleView },
     { path: '/article/:id', name: 'article', component: ArticleView },
     { path: '/article/:id/edit', name: 'edit-article', component: EditArticleView },
-    { path: '/user/:id', name: 'user', component: UserView },
-    { path: '/profile', name: 'profile', component: UserView },
     //GPT-WRITER
     { path: '/gpt-writer', name: 'gpt-writer', component: GptWriterView },
+    // USERS
+    { path: '/user/:id', name: 'user', component: UserView },
+    { path: '/profile', name: 'profile', component: UserView },
+    // NOT FOUND
     { path: '/:pathMatch(.*)*', component: PathNotFound }
   ]
 })
