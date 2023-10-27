017b535ed03ed98ee36064bf9731df01e9fa183e
desunovu <desunovu@gmail.com>
2023-10-27 20:29:10 +0300

    refactor(frontend): изменить пути в кнопках на имена страниц

--- frontend/src/components/HeaderMenu.vue
+++ frontend/src/components/HeaderMenu.vue
@@ -11,27 +11,28 @@ const props = defineProps<{ currentUser?: UserDocument }>()
         <img src="https://img.icons8.com/3d-fluency/94/disguised-face-1.png" />
       </q-avatar>
       <q-btn :to="{ name: 'home' }" stretch flat>fastapi-mongodb-vue-blog</q-btn>
-      <router-link to="/about" custom>by Desunovu</router-link>
+      <router-link to="{ name: 'about' }" custom>by Desunovu</router-link>
     </q-toolbar-title>
 
     <q-space />
 
     <div v-if="!currentUser" class="row">
-      <q-btn to="/login" stretch flat label="Войти" />
-      <q-btn to="/register" stretch flat label="Зарегестрироваться" />
+      <q-btn :to="{ name: 'login' }" stretch flat label="Войти" />
+      <q-btn :to="{ name: 'register' }" stretch flat label="Зарегестрироваться" />
     </div>
     <div v-else class="row">
       <!-- Блок который показывается если пользователь администратор или автор -->
       <div v-if="currentUser.role == 'Admin' || currentUser.role == 'Author'" class="text-accent">
-        <q-btn to="/create-article" label="Написать статью" flat />
+        <q-btn :to="{ name: 'create-article' }" label="Написать статью" flat />
       </div>
       <!-- Блок который показывается если пользователь администратор -->
       <div v-if="currentUser.role == 'Admin'" class="text-accent">
-        <q-btn to="/gpt-writer" label="GptWriter" flat />
+        <q-btn :to="{ name: 'gpt-writer' }" label="GptWriter" flat />
       </div>
-      <q-btn to="/" stretch flat label="Все статьи" />
-      <q-btn to="/profile" stretch flat>{{ currentUser.username }}</q-btn>
-      <q-btn to="/logout" stretch flat label="Выйти" />
+      <!-- Блок для всех пользователей -->
+      <q-btn :to="{ name: 'home' }" stretch flat label="Все статьи" />
+      <q-btn :to="{ name: 'profile' }" stretch flat>{{ currentUser.username }}</q-btn>
+      <q-btn :to="{ name: 'logout' }" stretch flat label="Выйти" />
     </div>
   </q-toolbar>
 </template>
